7983d9943a36467965ab909e3fb204805f63a1f5
Krish Sarkar <krish73071@outlook.com>
2026-03-05 11:55:46 -0800

    Refactor Vite config to simplify plugin usage
    Removed server configuration and updated plugin import.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    base: "/CSE108_Lab4/",
-    server: {
-        port: 63520,
-    }
-
+  plugins: [react()],
+  base: "/CSE108_Lab4/",
 })
